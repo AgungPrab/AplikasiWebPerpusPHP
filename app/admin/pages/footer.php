376bc7fb8619fb733f41a3d9973b763dd3eb3816
AgungPrab <122008045+AgungPrab@users.noreply.github.com>
2024-02-22 23:06:57 +0700

    Add files via upload

---
+++ app/admin/pages/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> 1.0
+    </div>
+    
+</footer>
---
+++ app/user/pages/footer.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> 1.0
+    </div>
+</footer>
